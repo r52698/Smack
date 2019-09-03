02c5cc9de4d809a23b69b39769d339083ba22e38
dror <drorpilot@gmail.com>
2019-09-03 13:47:15 +0300

    lesson 10

--- app/src/main/java/com/example/smack/Controller/CreateUserActivity.kt
+++ app/src/main/java/com/example/smack/Controller/CreateUserActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.example.smack.R
+import com.example.smack.Services.AuthService
 import kotlinx.android.synthetic.main.activity_create_user.*
 import java.util.*
 
@@ -44,6 +45,10 @@ class CreateUserActivity : AppCompatActivity() {
     }
 
     fun createUserClicked(view: View) {
-
+        AuthService.registerUser(this, "j@j.com", "123456") {complete ->
+            if (complete) {
+                println ("completed successfully")
+            }
+        }
     }
 }
