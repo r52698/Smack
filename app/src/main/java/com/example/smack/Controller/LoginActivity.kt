d51adb019d33e53c9897ee80a12e265a875a0e79
dror <drorpilot@gmail.com>
2019-09-04 22:14:49 +0300

    lesson 16

--- app/src/main/java/com/example/smack/Controller/LoginActivity.kt
+++ app/src/main/java/com/example/smack/Controller/LoginActivity.kt
@@ -1,10 +1,13 @@
 package com.example.smack.Controller
 
+import android.content.Context
 import android.content.Intent
 import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethod
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.example.smack.R
@@ -21,35 +24,55 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun loginLoginBtnClicked (view: View) {
-        loginSpinner.visibility = View.VISIBLE
+        enableSpinner(true)
         val email = loginEmailText.text.toString()
         val password = loginPasswordText.text.toString()
-        println("Trying to login email $email password $password")
-        AuthService.loginUser(this, email, password) { loginSuccess ->
-            if (loginSuccess) {
-                println("Login completed successfully")
-                AuthService.findUserByEmail(this) { findUserSuccess ->
-                    if (findUserSuccess) {
-                        println("Find user by email completed successfully")
-                        val userDataChange = Intent(BROADCAST_USER_DATA_CHANGE)
-                        LocalBroadcastManager.getInstance(this).sendBroadcast(userDataChange)
-                        finish()
-                    } else {
-                        errorToast()
-                        loginSpinner.visibility = View.INVISIBLE
-                        println("The find by email attempt was not successful")
+        hideKeyboard()
+        if (email.isNotEmpty() && password.isNotEmpty()) {
+            println("Trying to login email $email password $password")
+            AuthService.loginUser(this, email, password) { loginSuccess ->
+                if (loginSuccess) {
+                    println("Login completed successfully")
+                    AuthService.findUserByEmail(this) { findUserSuccess ->
+                        if (findUserSuccess) {
+                            println("Find user by email completed successfully")
+                            val userDataChange = Intent(BROADCAST_USER_DATA_CHANGE)
+                            LocalBroadcastManager.getInstance(this).sendBroadcast(userDataChange)
+                            finish()
+                        } else {
+                            errorToast()
+                            println("The find by email attempt was not successful")
+                        }
                     }
+                } else {
+                    println("The login attempt was not successful")
+                    errorToast()
                 }
-            } else {
-                println("The login attempt was not successful")
-                errorToast()
-                loginSpinner.visibility = View.INVISIBLE
             }
+        } else {
+            Toast.makeText(this, "Please fill in email and password.", Toast.LENGTH_LONG).show()
+            enableSpinner(false)
         }
     }
 
     fun errorToast() {
         Toast.makeText(this, "Something went wrong, please try again.", Toast.LENGTH_LONG).show()
+        enableSpinner(false)
+    }
+
+    fun enableSpinner(enable: Boolean) {
+        if (enable) {
+            loginSpinner.visibility = View.VISIBLE
+        } else {
+            loginSpinner.visibility = View.INVISIBLE
+        }
+        loginLoginBtn.isEnabled = !enable
+        loginCreateUSerBtn.isEnabled = !enable
+    }
+
+    fun hideKeyboard() {
+        val inputManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputManager.hideSoftInputFromWindow(currentFocus.windowToken, 0)
     }
 
     fun loginCreateUserBtnClicked (view: View) {
